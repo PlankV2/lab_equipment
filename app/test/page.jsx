69481033e132752a37098cf6c83a92d55b7549f7
PlankV2 <dev@erxes.io>
2025-11-19 14:11:46 +0800

    test page added

---
+++ app/test/page.jsx
@@ -0,0 +1,12 @@
+import LoginPrompt from '@/components/LoginPrompt'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <LoginPrompt/>
+    </div>
+  )
+}
+
+export default page
---
+++ components/LoginPrompt.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPrompt = () => {
+  return (
+    <div className="w-[100px] h-[100px] bg-amber-200">
+      tw
+    </div>
+  )
+}
+
+export default LoginPrompt
