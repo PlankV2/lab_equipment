47ce05f01e4a1f41edd40d71978f7fa402175db1
Nandintushig <nandintushig08@gmail.com>
2025-12-03 14:52:46 +0800

    Co-authored-by: Yeshuakyzilbash <randomdude1248@gmail.com>

--- components/LoginPrompt.jsx
+++ components/LoginPrompt.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 
 const LoginPrompt = () => {
   return (
-    <div className="w-[100px] h-[100px] bg-amber-200">
-      tw
+     <div className="flex items-center justify-center min-h-screen">
+  <div className="bg-gray-200 p-6 rounded-lg">
+    Sign in with Google
     </div>
-  )
+</div>
+  );
 }
+
 
 export default LoginPrompt
